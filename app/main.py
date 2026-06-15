2be2845c88e9b19086fa0f14c187505e3159e235
gzxfy <135656929+gzxfy@users.noreply.github.com>
2026-06-15 09:10:28 -0700

    The note now gets created and store in the database. Now will be adding validations and more tests

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import flash, render_template, Blueprint, request
+from flask import flash, redirect, render_template, Blueprint, request, session, url_for
 from .models import Note, db
 
 main_bp = Blueprint('main', __name__)
@@ -7,6 +7,7 @@ main_bp = Blueprint('main', __name__)
 def home():
     return render_template('home.html')
 
+# will add topics later
 @main_bp.route('/create', methods=['GET', 'POST'])
 def create():
     content = None  # Initialize content variable or any other logic needed before rendering the create page
@@ -14,13 +15,19 @@ def create():
 
     if request.method == 'POST':
         # Add logic to handle form submission or data processing
+        user_id = session.get("user_id")  # Retrieve the user_id from the session
+        if not user_id:
+            flash('User not logged in!')
+            return redirect(url_for('main.home'))
+        
         content = request.form.get('content', '')
         title = request.form.get('title', '')
+        
 
-        new_note = Note(title=title, content=content)
+        new_note = Note(user_id=user_id, title=title, content=content)
         db.session.add(new_note)
         db.session.commit()
+        flash('Note created successfully!')  # Add a flash message to indicate successful creation
+        return redirect(url_for('main.home'))
+    return render_template('create.html', content=content, title=title)  # Render the create page for GET requests
     
-
-    flash('Note created successfully!')  # Add a flash message to indicate successful creation
-    return render_template('home.html', content=content, title=title)
